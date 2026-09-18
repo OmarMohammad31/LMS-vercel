ff42cefece2db24cedaa36480f047e55543d74dc
Omar Mohammad Yousef <OmarMohammad31@hotmail.com>
2026-09-19 02:28:24 +0300

    Connect to MongoDB in index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,6 @@
-module.exports = require('../app');
+const connectDB = require('../config/db');
+const app = require('../app');
+
+connectDB().catch((err) => console.error('MongoDB connection error:', err));
+
+module.exports = app;
