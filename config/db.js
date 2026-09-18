c0652382fab569d877f8f6d2212d6be7cd812cac
Omar Mohammad Yousef <OmarMohammad31@hotmail.com>
2026-09-19 01:27:32 +0300

    update db config for vercel back-end deployment

--- config/db.js
+++ config/db.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+require('../models/CreditTransaction');
+
+let connectionPromise = null;
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('MongoDB connected');
+  if (mongoose.connection.readyState === 1) return;
+  if (!connectionPromise) {
+    connectionPromise = mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log('MongoDB connected'))
+      .catch((err) => {
+        connectionPromise = null;
+        throw err;
+      });
+  }
+  await connectionPromise;
 }
 
 module.exports = connectDB;
